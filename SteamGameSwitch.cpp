910b5258a39310e727d71ee4b156441a72bbb137
Eliay Daisudov <eliayeliay2001@gmail.com>
2021-03-21 20:37:35 +0200

    Add project files.

---
+++ SteamGameSwitch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+}
+
+/*
+* step 1: get module address
+* step 2: apply offset or signature scan
+* 
+* step 3: save original bytes
+* step 4: write string to codecave
+* step 5: patch instruction to "mov eax, *string location*"
+*/
