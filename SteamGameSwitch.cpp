95c309def17b7d6ff4f4d55465cf2b1942eace84
Eliay Daisudov <eliayeliay2001@gmail.com>
2021-11-29 19:23:40 +0200

    refactored to use different mechanism
    todo: retain commandline args
    todo: dont intercept internal steam startprocess calls

--- SteamGameSwitch.cpp
+++ SteamGameSwitch.cpp
@@ -7,9 +7,10 @@
 #pragma warning(disable:4996)
 
 // Used for unhooking
-const BYTE hardcoded_bytes[] = { 0x8B, 0x45, 0xF8, 0x85, 0xC0 };
+const BYTE unhook_bytes[] = { 0x8b, 0xff, 0x55, 0x8b, 0xec }; // ITS HARDCODED :( TRY TO FIX IT
+int logAndExit(std::string logMessage);
 
-void unhook(HANDLE handle, LPVOID toHook);
+BOOL unhook(HANDLE handle, LPVOID toHook);
 
 int main(int argc, char* argv[])
 {
@@ -22,119 +23,190 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	module steamclient = GetModule(pid, L"steamclient.dll");
+	module kerneldll = GetModule(pid, L"KERNEL32.DLL");
+
+	if (kerneldll.dwSize == 0) {
+		return logAndExit("couldn't find kernel32.dll");
+	}
 
 	// Get path of exe from user
 	// commandline or explorer prompt
-	std::string input(MAX_PATH, '\0');
+	std::wstring input(MAX_PATH, '\0');
 	if (argc > 1) {
-		input = argv[1];
+		//input = argv[1]; TODO: convert
 	}
 	else {
-		OPENFILENAMEA ofn = { };
+		OPENFILENAMEW ofn = { };
 		ofn.lStructSize = sizeof(ofn);
 		ofn.hwndOwner = NULL;
-		ofn.lpstrFilter = "Executables only\0*.exe;*.com\0\0";
+		ofn.lpstrFilter = L"Executables only\0*.exe;*.com\0\0";
 		ofn.lpstrFile = &input[0];
 		ofn.nMaxFile = MAX_PATH;
-		ofn.lpstrTitle = "Select Your Executable";
+		ofn.lpstrTitle = L"Select Your Executable";
 		ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;
 
-		GetOpenFileNameA(&ofn);
+		GetOpenFileNameW(&ofn);
 	}
 
-	// signature is 5 bytes away the real target to allow unhooking
-	const BYTE signature[] = {
-		0x56, 0x0F, 0x45, 0xC8, 0x8D, 0x45, 0xFC, 0x51, 0x68, '?', '?', '?', '?', 0x50
-	};
-	ADDRESS toHook = (ADDRESS)signatureScan(handle, steamclient, signature, sizeof(signature)).at(0) - 5;
-
-	// unhook and return original bytes if exe wasnt chosen
-	if (input.at(0) == NULL) {
-		unhook(handle, (LPVOID)toHook);
-		return 0;
-	}
-
-	std::string exe, path;
+	std::wstring exe, path;
 	auto pos = input.find_last_of('\\');
 	exe = input.substr(pos + 1);
 	path = input.substr(0, pos);
-	
 
-	BYTE* originalBytes = new BYTE[targetSize];
-	ReadProcessMemory(handle, (LPVOID)toHook, originalBytes, targetSize, NULL);
-	// if a hook already exists
-	if (originalBytes[0] == 0xe9) {
-		unhook(handle, (LPVOID)toHook);
-		originalBytes = (BYTE*)&hardcoded_bytes;
+	// search for CreateProcessW address in own kernel32.dll
+	const auto kernel32handle = GetModuleHandle("kernel32.dll");
+	if (!kernel32handle) {
+		return logAndExit("Couldn't get a handle to kernel32");
 	}
 
-	// Insert string
-	ADDRESS pathLocation = (ADDRESS)VirtualAllocEx(handle, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	pathLocation++; // otherwise steam crashes
+	const ADDRESS createProcessAddress = (ADDRESS)GetProcAddress(kernel32handle, "CreateProcessW");
+	if (!createProcessAddress) {
+		DWORD latestError = GetLastError();
+		// couldnt get address to CreateProcessW
+		return logAndExit("Couldn't get address to target function");
+	}
 
-	ADDRESS exeLocation = (ADDRESS)pathLocation + path.length() + 1;
-	WriteProcessMemory(handle, (LPVOID)pathLocation, path.c_str(), path.length(), NULL);
-	WriteProcessMemory(handle, (LPVOID)exeLocation, exe.c_str(), path.length(), NULL);
+	const auto CREATE_PROCESS_W_LENGTH = 10;
 
-	DWORD oldProtect;
-	VirtualProtectEx(handle, (LPVOID)toHook, targetSize, PAGE_EXECUTE_READWRITE, &oldProtect);
+	BYTE* originalCreateProcess = new BYTE[CREATE_PROCESS_W_LENGTH];
+	memcpy(originalCreateProcess, (LPVOID)createProcessAddress, CREATE_PROCESS_W_LENGTH);
 
-	
-	size_t caveSize = 50;
-	LPVOID cave = VirtualAllocEx(handle, NULL, caveSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-	
-	// Create hook
-	ADDRESS hookJmpOffset = (ADDRESS)cave - toHook - 0x5;
-	std::vector<BYTE> hookcode = {
+	// read the bytes on target process using the local address found above
+
+	DWORD old_CPW_Protect;
+	const auto vpres =
+		VirtualProtectEx(handle, (LPVOID)createProcessAddress, targetSize, PAGE_EXECUTE_READWRITE, &old_CPW_Protect);
+	if (!vpres) {
+		return logAndExit("Couldn't get permissions to CreateProcessW on target process");
+	}
+	re_read_bytes:
+	BYTE* createProcessBytes = new BYTE[CREATE_PROCESS_W_LENGTH];
+	ZeroMemory(createProcessBytes, CREATE_PROCESS_W_LENGTH);
+	const auto cpResult = ReadProcessMemory(handle, (LPVOID)createProcessAddress, createProcessBytes, CREATE_PROCESS_W_LENGTH, NULL);
+	if (!cpResult) {
+		return logAndExit("Couldn't read from target process");
+	}
+
+	// unhook and return original bytes if exe wasnt chosen
+	if (input.at(0) == NULL) {
+		return unhook(handle, (LPVOID)createProcessAddress);
+	}
+
+	const auto firstByte = createProcessBytes[0];
+	if (firstByte == 0xe9) {
+		// unhook
+		const auto unhookRet = unhook(handle, (LPVOID)createProcessAddress);
+		if (!unhookRet) {
+			return logAndExit("Failed to unhook existing hook");
+		}
+		goto re_read_bytes;
+	}
+
+	// inject path string
+	std::wstring fullPath = path + L"\\" + exe;
+
+	ADDRESS fullPathLocation = (ADDRESS)VirtualAllocEx(handle, NULL, fullPath.length(), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	fullPathLocation++; // otherwise steam crashes
+
+	ADDRESS onlyPathLocation = (ADDRESS)VirtualAllocEx(handle, NULL, path.length(), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	onlyPathLocation++;
+
+	const auto exeRet = WriteProcessMemory(handle, (LPVOID)fullPathLocation, fullPath.c_str(), fullPath.length(), NULL);
+	if (exeRet == 0) {
+		std::cout << "Something went wrong while writing path to steam process!\n";
+		return 1;
+	}
+
+	const auto pathRet = WriteProcessMemory(handle, (LPVOID)onlyPathLocation, path.c_str(), path.length(), NULL);
+	if (pathRet == 0) {
+		std::cout << "Something went wrong while writing path to steam process!\n";
+		return 1;
+	}
+
+	// perform the hook procedure
+
+	// first, create our function (modFunction = function that replaces the previous path string with our string)
+	ADDRESS modFunction = (ADDRESS)VirtualAllocEx(handle, NULL, 0x256, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+
+	std::vector<BYTE> modShellcode = {
+		0xc7, // 
+		0x44, // 
+		0x24, // mov DWORD PTR [esp+0x4],0x5
+		0x08, // 
+		(BYTE)((fullPathLocation >> (4 * 8)) & 0xFF),
+		(BYTE)((fullPathLocation >> (1 * 8)) & 0xFF),
+		(BYTE)((fullPathLocation >> (2 * 8)) & 0xFF),
+		(BYTE)((fullPathLocation >> (3 * 8)) & 0xFF),
+
+		0xc7, // 
+		0x44, // 
+		0x24, // mov DWORD PTR [esp+0x8],0x5
+		0x0C, // 
+		(BYTE)((fullPathLocation >> (4 * 8)) & 0xFF),
+		(BYTE)((fullPathLocation >> (1 * 8)) & 0xFF),
+		(BYTE)((fullPathLocation >> (2 * 8)) & 0xFF),
+		(BYTE)((fullPathLocation >> (3 * 8)) & 0xFF),
+	};
+
+	const auto relativeReturnJmp = (createProcessAddress) - (modFunction + modShellcode.size()) - 5;
+
+	std::vector<BYTE> returnJmpShellcode = {
 		0xe9,
-		(BYTE)((hookJmpOffset >> (4 * 8)) & 0xFF),
-		(BYTE)((hookJmpOffset >> (1 * 8)) & 0xFF),
-		(BYTE)((hookJmpOffset >> (2 * 8)) & 0xFF),
-		(BYTE)((hookJmpOffset >> (3 * 8)) & 0xFF),
+		(BYTE)((relativeReturnJmp >> (4 * 8)) & 0xFF),
+		(BYTE)((relativeReturnJmp >> (1 * 8)) & 0xFF),
+		(BYTE)((relativeReturnJmp >> (2 * 8)) & 0xFF),
+		(BYTE)((relativeReturnJmp >> (3 * 8)) & 0xFF),
 	};
 
-	// Create shellcode
-	std::vector<BYTE> shellcode = {
-		0xc7,
-		0x45,
-		0xf8,
-		(BYTE)((pathLocation >> (4 * 8)) & 0xFF),
-		(BYTE)((pathLocation >> (1 * 8)) & 0xFF),
-		(BYTE)((pathLocation >> (2 * 8)) & 0xFF),
-		(BYTE)((pathLocation >> (3 * 8)) & 0xFF),
-		// 0xC7 0x45 0xF8 *addr*
-		// mov [ebp-8], addr
-
-		0xbe,
-		(BYTE)((exeLocation >> (4 * 8)) & 0xFF),
-		(BYTE)((exeLocation >> (1 * 8)) & 0xFF),
-		(BYTE)((exeLocation >> (2 * 8)) & 0xFF),
-		(BYTE)((exeLocation >> (3 * 8)) & 0xFF),
-		// 0xBE *addr*
-		// mov esi, *addr*
-	};
-	// Create the stolen bytes
-	for (int i = 0; i < targetSize; i++) {
-		shellcode.push_back((BYTE)originalBytes[i]);
+	// write stolen bytes to mod function
+	const auto stolenWriteRet =
+		WriteProcessMemory(handle, (LPVOID)modFunction, createProcessBytes, 5, NULL);
+	if (!stolenWriteRet) {
+		return logAndExit("Failed writing stolen bytes to modding function");
 	}
-	// Create return
-	ADDRESS returnJmpOffset = (toHook + targetSize) - ((ADDRESS)cave + shellcode.size()) - 5;
-	shellcode.push_back(0xe9); 
-	shellcode.push_back((BYTE)((returnJmpOffset >> (4 * 8)) & 0xFF));
-	shellcode.push_back((BYTE)((returnJmpOffset >> (1 * 8)) & 0xFF));
-	shellcode.push_back((BYTE)((returnJmpOffset >> (2 * 8)) & 0xFF));
-	shellcode.push_back((BYTE)((returnJmpOffset >> (3 * 8)) & 0xFF));
 
-	// apply hook here
-	WriteProcessMemory(handle, (LPVOID)toHook, &hookcode[0], hookcode.size(), NULL); // apply hook
-	WriteProcessMemory(handle, (LPVOID)cave, &shellcode[0], shellcode.size(), NULL);  // write shellcode
+	// write shellcode to mod function
+	const auto modWriteRet =
+		WriteProcessMemory(handle, (LPVOID)(modFunction+5), &modShellcode[0], modShellcode.size(), NULL);
+	if (!modWriteRet) {
+		return logAndExit("Failed writing shellcode to modding function");
+	}
 
+	const auto returnJmpRet =
+		WriteProcessMemory(handle, (LPVOID)(modFunction + 5 + modShellcode.size()),
+			&returnJmpShellcode[0], returnJmpShellcode.size(), NULL);
+	if (!returnJmpRet) {
+		return logAndExit("Failed writing return shellcode to modding function");
+	}
 
-	VirtualProtectEx(handle, (LPVOID)toHook, targetSize, oldProtect, &oldProtect);
+	// overwrite jmp
+	const int target = createProcessAddress;
+	const auto relativeJmp = modFunction - target - 5;
+
+	std::vector<BYTE> hookShellcode = {
+		0xE9,
+		(BYTE)((relativeJmp >> (4 * 8)) & 0xFF),
+		(BYTE)((relativeJmp >> (1 * 8)) & 0xFF),
+		(BYTE)((relativeJmp >> (2 * 8)) & 0xFF),
+		(BYTE)((relativeJmp >> (3 * 8)) & 0xFF),
+	};
+
+	const auto overwriteJmpRet =
+		WriteProcessMemory(handle, (LPVOID)target, &hookShellcode[0], hookShellcode.size(), NULL);
+	if (!overwriteJmpRet) {
+		return logAndExit("Failed to hook");
+	}
+
+	// cleanup
+	VirtualProtectEx(handle, (LPVOID)createProcessAddress, targetSize, old_CPW_Protect, &old_CPW_Protect);
 }
 
-void unhook(HANDLE handle, LPVOID toHook) {
-	
-	WriteProcessMemory(handle, toHook, &hardcoded_bytes, sizeof(hardcoded_bytes), NULL);
+BOOL unhook(HANDLE handle, LPVOID toUnhook) {
+	return WriteProcessMemory(handle, toUnhook, &unhook_bytes, sizeof(unhook_bytes), NULL);
+}
+
+int logAndExit(std::string logMessage) {
+	std::cout << logMessage << "\n";
+	return 1;
 }
